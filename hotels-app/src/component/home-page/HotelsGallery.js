415e8e4713f7836b829c2e837e4f6d695dcfcb7c
zivserphos <zivfromisrael@gmail.com>
2021-11-21 13:54:09 +0200

    basic structure

---
+++ hotels-app/src/component/home-page/HotelsGallery.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Hotel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Hotel;
